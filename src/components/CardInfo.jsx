59eae1441d1732930e8d4d1f827cec3fbe770585
Efren Zamora <efrenzc@ibushak.com>
2023-10-29 17:33:09 -0600

    Images updated

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import IconCheck from "/src/assets/icon-check.svg"
+
 export const CardInfo = () => {
     return (
         <section className='card__info'>
             <div className="card__info_features_check">
                 <div className='card__info_features'>
-                    <img src="../src/assets/icon-check.svg" alt="checkIcon" />
+                    <img src={IconCheck} alt="checkIcon" />
                     <p>Unlimited websites</p>
                 </div>
                 <div className='card__info_features'>
-                    <img src="../src/assets/icon-check.svg" alt="checkIcon" />
+                    <img src={IconCheck} alt="checkIcon" />
                     <p>100% data ownership</p>
                 </div>
                 <div className='card__info_features'>
-                    <img src="../src/assets/icon-check.svg" alt="checkIcon" />
+                    <img src={IconCheck} alt="checkIcon" />
                     <p>Email reports</p>
                 </div>
-
             </div>
             <div className="card__info_btn">
                 <a href="" className='btn text-center'>Start my trial</a>
